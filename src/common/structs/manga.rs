3ccee6c0a5ddb0826997be7dcdf11d84d5a2777f
erdemgoksel <erdemgoksel@MAU-BILISIM42>
2026-02-10 16:41:24 +0300

    Revert "Expand MangaExtended and update get_manga_full"
    This reverts commit 4288e386b269a58f5c2e66b71047af88cce79466.

--- src/common/structs/manga.rs
+++ src/common/structs/manga.rs
@@ -1,6 +1,6 @@
 use crate::{
     response::MalCommonTypeResponse,
-    utils::{DateRange, Images, Score, Title, ExternalEntry},
+    utils::{DateRange, Images, Score, Title},
 };
 use serde::{Deserialize, Serialize};
 
@@ -45,8 +45,6 @@ pub struct MangaExtended {
     pub favorites: Option<u32>,
     pub synopsis: Option<String>,
     pub background: Option<String>,
-    pub relations: Option<Vec<MangaRelation>>,     
-    pub external: Option<Vec<ExternalEntry>>,
     pub authors: Vec<MalCommonTypeResponse>,
     pub serializations: Vec<MalCommonTypeResponse>,
     pub genres: Vec<MalCommonTypeResponse>,
